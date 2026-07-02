ad24c3e0bb887339b8bb3f36669fa289432d8a6d
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-07-02 20:48:41 +0300

    Finished Work!!!

--- README.md
+++
@@ -1,2 +0,0 @@
-# Assignment4_Postgress_Loran_Mariia
-Design and implement an operational PostgreSQL database for a particular application.
--- create_view.sql
+++ create_view.sql
@@ -1,6 +1,12 @@
 CREATE view hms.active_booking_report AS
-SELECT b.IDBooking  ,g.first_name, g.last_name, r.room_number, b.arrival_date
+SELECT b.IDBooking  ,
+       g.first_name,
+       g.last_name,
+       r.room_number,
+       b.arrival_date
 FROM hms.Booking b
 JOIN hms.Guests g ON b.IDGuest = g.IDGuest
 JOIN hms.Room r ON b.IDRoom = r.IDRoom;
 
+
+SELECT * FROM hms.active_booking_report;
